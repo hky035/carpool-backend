2cae23c5465b6ffdbbf26ff127f29c9e3ac7ab62
hky035 <rldud035@naver.com>
2024-06-05 02:44:30 +0900

    feat: proxy 설정

--- src/main/java/com/oss/carpool/CarpoolApplication.java
+++ src/main/java/com/oss/carpool/CarpoolApplication.java
@@ -2,6 +2,9 @@ package com.oss.carpool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CarpoolApplication {
@@ -9,5 +12,16 @@ public class CarpoolApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarpoolApplication.class, args);
 	}
-
+	
+	@Bean
+	  public WebMvcConfigurer corsConfigurer(){
+	      return new WebMvcConfigurer(){
+	          @Override
+	          public void addCorsMappings(CorsRegistry registry){
+	              registry.addMapping("/**")  // 특정 패턴에 대해 CORS 처리가 가능하도록 함 . 우리는 모든 것에 대해 활성화
+	                      .allowedMethods("*") // GET, PUT과 같은 모든 메서드 허용
+	                      .allowedOrigins("http://localhost:3000"); 
+	          }
+	      };
+	  }
 }
---
+++ src/main/java/com/oss/carpool/WebConfig.java
@@ -0,0 +1,15 @@
+//package com.oss.carpool;
+//
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//public class WebConfig implements WebMvcConfigurer{
+//
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//			registry.addMapping("/**")
+//			.allowedOrigins("http://localhost:3000")
+//			.allowedMethods("OPTIONS","GET","POST","PUT","DELTE");
+//	}
+//	
+//}
