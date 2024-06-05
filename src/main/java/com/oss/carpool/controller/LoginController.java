e9a1e3688da0c763eb0bc6825c9d46f290244346
hky035 <rldud035@naver.com>
2024-06-06 02:03:08 +0900

    refactor: 불필요 import 패키지 삭제

--- src/main/java/com/oss/carpool/controller/LoginController.java
+++ src/main/java/com/oss/carpool/controller/LoginController.java
@@ -1,12 +1,10 @@
 package com.oss.carpool.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oss.carpool.domain.User;
