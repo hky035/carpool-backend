062f723bf0655ee23cda533b3b8cb2557997f68e
hky035 <rldud035@naver.com>
2024-06-06 18:01:05 +0900

    feat: 초기 데이터 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
 INSERT INTO user_details (userId, password, studentNumber, mileage) VALUES ('user1', 'password1', 123456, 0);
 
-INSERT INTO post (author_id, title, description) VALUES (1, '제목 1', '설명 1');
+
+INSERT INTO post (author_id, title, description) VALUES (1, '카풀 서비스 거리 확대 요청', '카풀 서비스 이용 가능 거리가 확대되었으면 좋겠습니다.');
+
+INSERT INTO post (author_id, title, description) VALUES (1, '마일리지 학업 관련 서비스 추가 요청', '학업 관련 마일리지 서비스가 도입되면 좋겠습니다.');
+
+INSERT INTO post (author_id, title, description) VALUES (1, '마일리지 획득량 변동 요구', '카풀 서비스 이용 시 마일리지가 더 많이 쌓였으면 좋겠습니다.');
+
+INSERT INTO post (author_id, title, description) VALUES (1, '클리커 앱 또는 출결앱 등과 연동 요규', '다른 교내 서비스 플랫폼과 연동이 되었으면 합니다.');
