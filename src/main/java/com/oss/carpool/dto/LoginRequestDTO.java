1f4b816044ac183b68580b9773ef0727f2bcbb6e
hky035 <rldud035@naver.com>
2024-06-13 03:20:50 +0900

    feat: lombok @Getter @Setter 추가

--- src/main/java/com/oss/carpool/dto/LoginRequestDTO.java
+++ src/main/java/com/oss/carpool/dto/LoginRequestDTO.java
@@ -1,17 +1,12 @@
 package com.oss.carpool.dto;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @RequiredArgsConstructor
+@Getter @Setter
 public class LoginRequestDTO {
 	private String userId;
-	private String password;
-	public String getUserId() {
-		return userId;
-	}
-	public String getPassword() {
-		return password;
-	}
-	
-	
+	private String password;	
 }
