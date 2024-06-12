077bd0b06f8257435e35f5f5e8c7835975e9d696
hky035 <rldud035@naver.com>
2024-06-13 03:17:53 +0900

    feat: 초기 마일리지 1000으로 설정

--- src/main/java/com/oss/carpool/domain/User.java
+++ src/main/java/com/oss/carpool/domain/User.java
@@ -4,20 +4,18 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.annotation.Nullable;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
-
 
 @Entity(name="user_details")
 @Getter
@@ -33,21 +31,17 @@ public class User {
 	private Integer studentNumber;
 	
 	@OneToOne(mappedBy = "provider")
-	@Nullable
 	@JsonIgnore
 	private Carpool provideCarpool;
-	@ManyToOne
-	@Nullable
-	@JoinColumn(name = "use_carpool_id")
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JsonIgnore
 	private Carpool useCarpool;
 	
 
 	@OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Post> posts;
-	
-//	@OneToMany
-//	private List<Review> reviews;
+
 	private Integer mileage;
 	
 	public User() {};
@@ -56,7 +50,7 @@ public class User {
 		this.userId = userId;
 		this.password = password;
 		this.studentNumber = studentNumber;
-		mileage = 0;
+		mileage = 1000;
 	}
 	
 }
