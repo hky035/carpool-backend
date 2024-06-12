02e5807c53554182667022bcb7780086894d687f
hky035 <rldud035@naver.com>
2024-06-13 03:14:33 +0900

    feat: registerCarpool() 수정

--- src/main/java/com/oss/carpool/controller/CarpoolController.java
+++ src/main/java/com/oss/carpool/controller/CarpoolController.java
@@ -5,9 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oss.carpool.domain.Carpool;
+import com.oss.carpool.dto.AddCarpoolRequestDTO;
+import com.oss.carpool.dto.CarpoolResponseDTO;
+import com.oss.carpool.dto.RegisterCarpoolRequestDTO;
 import com.oss.carpool.service.CarpoolService;
 
 @RestController
@@ -20,13 +25,22 @@ public class CarpoolController {
 	}
 	
 	@GetMapping("/api/carpool")
-	public List<Carpool> getAllCarpool(){
+	public List<CarpoolResponseDTO> getAllCarpool(){
 		return carpoolService.getAllCarpool();	
 	}
 	
 	@GetMapping("/api/carpool/{id}")
-	public Carpool getCarpool(@PathVariable("id") Long id) {
+	public CarpoolResponseDTO getCarpool(@PathVariable("id") Long id) {
 		return carpoolService.getCarpool(id);
 	}
 	
+	@PostMapping("/api/carpool/add")
+	public Carpool addCarpool(@RequestBody AddCarpoolRequestDTO dto) {
+		return carpoolService.addCarpool(dto);
+	}
+	
+	@PostMapping("/api/carpool/register")
+	public CarpoolResponseDTO registerCarpool(@RequestBody RegisterCarpoolRequestDTO dto) {
+		return carpoolService.registerCarpool(dto);
+	}
 }
