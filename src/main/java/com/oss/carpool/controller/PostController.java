8e2453412187f80aee7d808443cd66bc48eb8736
hky035 <rldud035@naver.com>
2024-06-13 12:31:06 +0900

    feat: addPost 반환값 수정

--- src/main/java/com/oss/carpool/controller/PostController.java
+++ src/main/java/com/oss/carpool/controller/PostController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.oss.carpool.domain.Post;
 import com.oss.carpool.dto.AddPostRequestDTO;
-import com.oss.carpool.dto.AllPostResponseDTO;
+import com.oss.carpool.dto.PostResponseDTO;
 import com.oss.carpool.service.PostService;
 
 @RestController
@@ -24,20 +24,12 @@ public class PostController {
 	}
 	
 	@GetMapping("api/post")
-	public List<AllPostResponseDTO> getPost(){
+	public List<PostResponseDTO> getAllPost(){
 		return postService.getAllPost();
 	}
 	
 	@PostMapping("/api/post/add")
-	public boolean addPost(@RequestBody AddPostRequestDTO dto) {
-		
-		Post newPost = postService.addNewPost(dto);
-		
-		if(newPost != null)
-			return true;
-		else
-			return false;
+	public PostResponseDTO addPost(@RequestBody AddPostRequestDTO dto) {
+		return postService.addPost(dto);
 	}
-	
-	
 }
